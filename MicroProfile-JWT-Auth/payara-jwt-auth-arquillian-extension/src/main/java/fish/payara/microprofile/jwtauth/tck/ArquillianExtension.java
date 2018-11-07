b125f6a56a3df6d9807a7dcc642dffd747ea9a61
merorai <susan.rai04@gmail.com>
2018-11-07 17:34:35 +0000

    Support for MicroProfile 1.3 Release

--- MicroProfile-JWT-Auth/payara-jwt-auth-arquillian-extension/src/main/java/fish/payara/microprofile/jwtauth/tck/ArquillianExtension.java
+++ MicroProfile-JWT-Auth/payara-jwt-auth-arquillian-extension/src/main/java/fish/payara/microprofile/jwtauth/tck/ArquillianExtension.java
@@ -41,6 +41,7 @@ package fish.payara.microprofile.jwtauth.tck;
 
 import org.jboss.arquillian.container.test.spi.client.deployment.ApplicationArchiveProcessor;
 import org.jboss.arquillian.core.spi.LoadableExtension;
+import org.jboss.arquillian.test.spi.enricher.resource.ResourceProvider;
 
 /**
  * This arquillian extension installs the {@link ArquillianArchiveProcessor}, which
@@ -56,7 +57,11 @@ public class ArquillianExtension implements LoadableExtension {
     public void register(ExtensionBuilder extensionBuilder) {
         System.err.println("\n\n\n\n Registered Payara TCK ArquillianExtension \n\n\n\n");
         
-        extensionBuilder.service(ApplicationArchiveProcessor.class, ArquillianArchiveProcessor.class);
+        extensionBuilder.service(ApplicationArchiveProcessor.class, ArquillianArchiveProcessor.class)
+                        .override(
+                                ResourceProvider.class,
+                                org.jboss.arquillian.container.test.impl.enricher.resource.URLResourceProvider.class,
+                                fish.payara.microprofile.jwtauth.tck.URLResourceProvider.class);
     }
 
 }
