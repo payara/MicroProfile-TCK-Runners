c0b95d96e98d2b1caf89d92c96308ed94fd3add3
Cuba Stanley <cuba.stanley@shibadogstudios.com>
2022-11-04 17:00:29 +0000

    Post step cleanup

--- .jenkins/modules/Test/Test.groovy
+++ .jenkins/modules/Test/Test.groovy
@@ -1,3 +1,12 @@
+MPLPostStep('always') {
+    cleanWs()
+}
+
+MPLPostStep('failure') {
+    echo "There are test failures, archiving server log"
+    archiveArtifacts artifacts: "./${${getPayaraDirectoryName}}/glassfish/domains/${getDomainName()}/logs/server.log"
+}
+
 withMaven(jdk: CFG.jdk, options: [artifactsPublisher(disabled: true)]) {
     sh """mvn -B -V -ff -e clean verify --strict-checksums \
         -Djavax.net.ssl.trustStore=${env.JAVA_HOME}/jre/lib/security/cacerts \
