a89bffeeeba32104fcc51d3f503ea66a6365c99b
Kalin Chan <kalin.chan@payara.fish>
2022-11-29 14:43:43 +0000

    DEVOPS-722 Wrap maven

--- .jenkins/modules/Setup/TestSetup.groovy
+++ .jenkins/modules/Setup/TestSetup.groovy
@@ -6,8 +6,10 @@ def payaraVersion = CFG.params.payaraVersion
 def distributionName = "${getDistributionName()}"
 def groupId = (getDistributionName().equals('micro')) ? 'fish.payara.extras' : 'fish.payara.distributions'
 def fileExtension = (getDistributionName().equals('micro')) ? 'jar' : 'zip'
-sh "mv ${distributionName}.${fileExtension} ${distributionName}-${payaraVersion}.${fileExtension}"
-sh "mvn install:install-file -DgroupId=${groupId} -DartifactId=${distributionName} -Dversion=${payaraVersion} -Dpackaging=${fileExtension} -Dfile=${pwd()}/${distributionName}-${params.payaraVersion}.${fileExtension}"
+withMaven(jdk: CFG.jdk) {
+    sh "mv ${distributionName}.${fileExtension} ${distributionName}-${payaraVersion}.${fileExtension}"
+    sh "mvn install:install-file -DgroupId=${groupId} -DartifactId=${distributionName} -Dversion=${payaraVersion} -Dpackaging=${fileExtension} -Dfile=${pwd()}/${distributionName}-${params.payaraVersion}.${fileExtension}"
+}
 
 def getDistributionName() {
     switch (CFG.params.distribution) {
